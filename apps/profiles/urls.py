5485f41704991a6d529dd96a579725dbb1724218
manulangat <emmanuelthedeveloper@gmail.com>
2022-08-06 23:00:27 +0300

    feat  : profile serializers and api endpoints

---
+++ apps/profiles/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (
+    AgentListAPIView,
+    GetProfileAPIView,
+    TopAgentsListAPIView,
+    UpdateProfileAPIView,
+)
+
+
+urlpatterns = [
+    path("agents/all/", AgentListAPIView.as_view(), name="agent-list"),
+    path("top-agents/all/", TopAgentsListAPIView.as_view(), name="top-agents"),
+    path("me/", GetProfileAPIView.as_view(), name="get-profile", name="Get profile"),
+    path("update/<str:username>/", UpdateProfileAPIView.as_view(), name="update-profile"),
+]
